3232ee146cd5516ef2b4c5a6cbfefedbe5f4702a
Rosendito <38988693+Rosendito@users.noreply.github.com>
2019-05-08 11:20:35 -0400

    Cambios

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('css/materialdesign-icons.css') }}">
 
     {{-- Admin Style --}}
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-assets/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin/style.css') }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 </head>
@@ -27,6 +27,6 @@
 
     </script>
     <script type="text/javascript" src="{{ asset('vendor/ckeditor/ckeditor.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('admin-assets/app.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('admin/app.js') }}"></script>
 </body>
 </html>
