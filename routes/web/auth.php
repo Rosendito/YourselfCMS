92c6dcab2596b8b7f5aad1c12d6be3c6277ab757
Rosendito <38988693+Rosendito@users.noreply.github.com>
2019-06-27 07:56:30 -0400

    Cambiando prefijo de admin a cms en las rutas de login

--- routes/web/auth.php
+++ routes/web/auth.php
@@ -8,6 +8,6 @@
 | Routes for Authentication views and logic
 */
 
-Route::get('admin/login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('admin/login', 'Auth\LoginController@login');
-Route::get('admin/logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('cms/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('cms/login', 'Auth\LoginController@login');
+Route::get('cms/logout', 'Auth\LoginController@logout')->name('logout');
